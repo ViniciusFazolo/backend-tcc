0a34e887131b102f48880322c52c04051eaf3acb
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2025-09-05 13:03:05 -0300

    feat: classe com constantes

---
+++ src/main/java/com/backend/tcc/constants/Contants.java
@@ -0,0 +1,7 @@
+package com.backend.tcc.constants;
+
+public class Contants {
+    public static final String GROUP_NOIMAGE_URL = "https://res.cloudinary.com/dthfiud4j/image/upload/v1757087461/groupNoImage_uqcg0c.png";
+    public static final String USER_NOIMAGE_URL = "https://res.cloudinary.com/dthfiud4j/image/upload/v1757087466/userNoImage_mdeztr.png";
+    public static final String ALBUM_NOIMAGE_URL = "https://res.cloudinary.com/dthfiud4j/image/upload/v1756137688/kdhi6rbmbcet1780nbo3.png";
+}
