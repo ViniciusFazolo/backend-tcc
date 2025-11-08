412642e6981631899e99085c0513e06356160ccd
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2025-11-08 15:12:10 -0300

    feat: implementa endpoint de exclusão de álbum

--- src/main/java/com/backend/tcc/controller/AlbumController.java
+++ src/main/java/com/backend/tcc/controller/AlbumController.java
@@ -1,14 +1,15 @@
 package com.backend.tcc.controller;
 
 import java.util.List;
-import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,5 +38,10 @@ public class AlbumController {
     @GetMapping("/group/{groupId}")
     public ResponseEntity<List<AlbumResponseDTO>> findByGroupId(@PathVariable String groupId) {
         return ResponseEntity.ok().body(service.findByGroupId(groupId));
+    }
+    
+    @DeleteMapping
+    public void delete(@RequestBody List<String> ids) {
+        service.delete(ids);
     }
 }
--- src/main/java/com/backend/tcc/services/AlbumService.java
+++ src/main/java/com/backend/tcc/services/AlbumService.java
@@ -6,12 +6,15 @@ import org.springframework.stereotype.Service;
 
 import com.backend.tcc.constants.Constants;
 import com.backend.tcc.domain.group.album.Album;
+import com.backend.tcc.domain.image.Images;
 import com.backend.tcc.dto.group.album.AlbumRequestDTO;
 import com.backend.tcc.dto.group.album.AlbumResponseDTO;
 import com.backend.tcc.exceptions.PadraoException;
 import com.backend.tcc.mapper.AlbumMapper;
 import com.backend.tcc.repositories.AlbumRepository;
+import com.backend.tcc.repositories.ImageRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -19,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 public class AlbumService {
     private final AlbumMapper mapper;
     private final AlbumRepository repository;
+    private final ImageRepository imageRepository;
     private final CloudinaryService cloudinaryService;
 
     public AlbumResponseDTO save(AlbumRequestDTO request) {
@@ -48,5 +52,38 @@ public class AlbumService {
         return repository.findByGroupId(groupId).stream()
                 .map(mapper::toDto)
                 .toList();
+    }
+
+    @Transactional
+    public void delete(List<String> ids) {
+        List<Album> albums = repository.findAllById(ids);
+        List<Images> images = imageRepository.findAllByPublish_AlbumIdIn(ids);
+
+        try {
+            for (Album album : albums) {
+                if (album.getImage() != null && !album.getImage().equals(Constants.ALBUM_NOIMAGE_URL)) {
+                    try {
+                        cloudinaryService.deleteFileByUrl(album.getImage());
+                    } catch (Exception ex) {
+                        System.err.println("Erro ao deletar imagem do álbum: " + ex.getMessage());
+                    }
+                }
+            }
+
+            for (Images image : images) {
+                if (image.getImage() != null) {
+                    try {
+                        cloudinaryService.deleteFileByUrl(image.getImage());
+                    } catch (Exception ex) {
+                        System.err.println("Erro ao deletar imagem: " + ex.getMessage());
+                    }
+                }
+            }
+
+            repository.deleteAll(albums);
+
+        } catch (Exception e) {
+            throw new PadraoException("Erro ao excluir álbum, tente novamente");
+        }
     }
 }
