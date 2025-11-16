a6c9a56b04b66d1a4aa0391afdcf54fb625e3508
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2025-11-16 10:13:51 -0300

    feat: seta a data do comentário realizado e atualiza a quantidade de comentários da publicação

--- src/main/java/com/backend/tcc/services/CommentaryService.java
+++ src/main/java/com/backend/tcc/services/CommentaryService.java
@@ -1,15 +1,18 @@
 package com.backend.tcc.services;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.backend.tcc.domain.commentary.Commentary;
+import com.backend.tcc.domain.publish.Publish;
 import com.backend.tcc.dto.commentary.CommentaryRequestDTO;
 import com.backend.tcc.dto.commentary.CommentaryResponseDTO;
 import com.backend.tcc.exceptions.PadraoException;
 import com.backend.tcc.mapper.CommentaryMapper;
 import com.backend.tcc.repositories.CommentaryRepository;
+import com.backend.tcc.repositories.PublishRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 public class CommentaryService {
     private final CommentaryRepository repository;
     private final CommentaryMapper mapper;
+    private final PublishRepository publishRepository;
 
     public List<CommentaryResponseDTO> findAll() {
         return repository.findAll().stream()
@@ -38,9 +42,18 @@ public class CommentaryService {
     }
 
     public CommentaryResponseDTO save(CommentaryRequestDTO request) {
+        Publish publish = publishRepository.findById(request.publish())
+                .orElseThrow(() -> new PadraoException("Publicação não encontrada"));
+
         try {
             Commentary entity = mapper.toEntity(request);
-            return mapper.toDto(repository.save(entity));
+            entity.setWhenSent(LocalDateTime.now());
+            entity = repository.save(entity);
+
+            publish.setQtCommentary(publish.getQtCommentary() + 1);
+            publishRepository.save(publish);
+
+            return mapper.toDto(entity);
         } catch (Exception e) {
             throw new PadraoException("Erro ao criar comentário");
         }
